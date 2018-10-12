f84f8336698b7ca7dfb77b03ff2b586b03d2070b
matianfu <matianfu@gmail.com>
2018-10-12 10:22:03 +0000

    remove CIPHER and DECIPHER

--- test.js
+++ test.js
@@ -22,7 +22,6 @@ TLS.createConnection({
 
 }, (err, tls) => {
   console.log('tls connected')
-
   tls.once('data', data => {
     if (data.equals(MQTT_PINGRESP)) {
       console.log('MQTT_PINGESP received')
@@ -34,3 +33,25 @@ TLS.createConnection({
   tls.write(MQTT_PINGREQ) 
   console.log('MQTT_PINGREQ sent to server')
 })
+
+/**
+let ca = fs.readFileSync('ca.pem').toString().replace(/\r\n/g, '\n')
+let clientCerificates = [
+]
+let clientPrivateKey = fs.readFileSync('deviceCert.key')
+
+let tls = new TLS({ ca, clientCertificates, clientPrivateKey })
+tls.on('error', err => console.log(err))
+tls.connect(8883,'a3dc7azfqxif0n.iot.cn-north-1.amazonaws.com.cn', () => {
+  tls.once('data', data => {
+    if (data.equals(MQTT_PINGRESP)) {
+      console.log('MQTT_PINGRESP received')
+    } else {
+      console.log('unexpected server response: ', data)
+    }
+  }) 
+  tls.write(MQTT_PINGREQ)
+  console.log('MQTT_PINGREQ sent to server')
+})
+*/
+
