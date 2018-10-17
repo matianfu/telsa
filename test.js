5d08ae222d11358873e7ea025a2b06e1f3fd607b
matianfu <matianfu@gmail.com>
2018-10-17 10:10:55 +0000

    lazy

--- test.js
+++ test.js
@@ -18,15 +18,31 @@ let clientPrivateKey = fs.readFileSync('deviceCert.key')
 let tls = new TLS({ ca, clientCertificates, clientPrivateKey })
 
 tls.on('error', err => console.log(err))
+tls.on('end', () => console.log('tls end'))
+tls.on('finish', () => console.log('tls finish'))
+tls.on('close', () => console.log('tls close'))
+
+let count = 0
 tls.connect(8883,'a3dc7azfqxif0n.iot.cn-north-1.amazonaws.com.cn', () => {
-  tls.once('data', data => {
+  tls.on('data', data => {
     if (data.equals(MQTT_PINGRESP)) {
       console.log('MQTT_PINGRESP received')
     } else {
       console.log('unexpected server response: ', data)
     }
+
+    if (++count === 3) {
+      setTimeout(() => {
+        tls.end()
+        setTimeout(() => {}, 3000)
+      }, 2000)
+    } else {
+      tls.write(MQTT_PINGREQ)
+      console.log('MQTT_PINGREQ sent to server', count)
+    }
   }) 
+
   tls.write(MQTT_PINGREQ)
-  console.log('MQTT_PINGREQ sent to server')
+  console.log('MQTT_PINGREQ sent to server', count)
 })
 
